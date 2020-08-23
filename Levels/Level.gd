50dd849ceb9bbcc8dcf69dfa6dd521b9c32b618e
svntax <endless.answers@gmail.com>
2020-08-22 23:44:56 -0400

    Added first level theme.

--- Levels/Level.gd
+++ Levels/Level.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
 onready var level_finished_ui = $UILayer/LevelFinishedUI
+onready var theme = $ThemeSong
+
+func _ready():
+	theme.play()
 
 func finish_level():
+	theme.stop()
 	level_finished_ui.show_ui()
